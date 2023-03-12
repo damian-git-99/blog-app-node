bc47e3dc7d6871484f03cfd98e3b7d69b2363435
damian <damiangalvan66@gmail.com>
2023-03-12 16:34:46 -0600

    feat(frontend): add layout page to avoid putting the navbar on all the pages

---
+++ react-frontend/src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar';
+import { Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
