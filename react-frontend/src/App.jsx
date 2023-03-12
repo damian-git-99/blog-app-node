b4fecd2c28efa60d6f45f31be0099f302d218342
damian <damiangalvan66@gmail.com>
2023-03-12 12:32:45 -0600

    feat(frontend): add home page with navbar and post entries

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import { Container, Row } from 'react-bootstrap';
+import { PostEntry } from './components/PostEntry';
+import { NavBar } from './components/NavBar';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <main>
-      app
+    <main className="App mt-5">
+      <header className="container">
+        <NavBar />
+      </header>
+
+      <Container className="mt-5 p-0 p-md-3">
+        <Row>
+          <PostEntry />
+          <PostEntry />
+          <PostEntry />
+        </Row>
+      </Container>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
