8013852c8e350d3d4b0b6c205b36da2fd7d5b1b6
damian <damiangalvan66@gmail.com>
2023-03-13 11:34:29 -0600

    fix(backend): cors error fixed

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -8,7 +8,8 @@ const app = express();
 const port = 4000;
 const corsOptions = {
   origin: 'http://127.0.0.1:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+  credentials: true
 };
 
 connectDB();
