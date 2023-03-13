5ff21b9319483586c93313d7b9f29498950f4600
damian <damiangalvan66@gmail.com>
2023-03-13 15:37:34 -0600

    feat(frontend): add user info in navbar

--- react-frontend/src/components/NavBar.jsx
+++ react-frontend/src/components/NavBar.jsx
@@ -1,31 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { userProfile } from '../api/userApi';
+import { logout } from '../api/authApi';
 
 export const NavBar = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    userProfile()
+      .then(data => {
+        setUser(data);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout();
+    setUser(null);
+  };
+
   return (
     <header className="container">
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">Blog</Link>
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav" 
-            aria-controls="navbarNav" 
-            aria-expanded="false" 
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
             aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to={'/login'}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/register'}>Register</Link>
-              </li>
-            </ul>
+            {user && (
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/login'}>Create new Post</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/login'}>{user.email}</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" onClick={handleLogout}>Logout</Link>
+                  </li>
+               </ul> 
+            )}
+            {!user && (
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/login'}>Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/register'}>Register</Link>
+                  </li>
+               </ul> 
+            )}
           </div>
         </div>
       </nav>
