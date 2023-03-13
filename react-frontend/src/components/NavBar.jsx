0b5081fb3b6c3fd108ffa444bd060f3aa7e4fa7a
damian <damiangalvan66@gmail.com>
2023-03-13 09:17:00 -0600

    fix(frontend): class to className fixed

--- react-frontend/src/components/NavBar.jsx
+++ react-frontend/src/components/NavBar.jsx
@@ -4,26 +4,26 @@ import { Link } from 'react-router-dom'
 export const NavBar = () => {
   return (
     <header className="container">
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/">Blog</Link>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Blog</Link>
           <button 
-            class="navbar-toggler" 
+            className="navbar-toggler" 
             type="button" 
             data-bs-toggle="collapse" 
             data-bs-target="#navbarNav" 
             aria-controls="navbarNav" 
             aria-expanded="false" 
             aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <Link class="nav-link" to={'/login'}>Login</Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to={'/login'}>Login</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link" to={'/register'}>Register</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/register'}>Register</Link>
               </li>
             </ul>
           </div>
