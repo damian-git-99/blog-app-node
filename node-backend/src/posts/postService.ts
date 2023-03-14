8b28974e7ff6a0d774c13cb69ff7bf3e9871e167
damian <damiangalvan66@gmail.com>
2023-03-14 17:53:33 -0600

    feat(backend): now the image is saved in cloudinary

--- node-backend/src/posts/postService.ts
+++ node-backend/src/posts/postService.ts
@@ -1,8 +1,12 @@
+import { uploadImage } from '../file/fileService';
 import { Post, PostModel } from './PostModel';
 
 export const cratePost = async (userId: string, post: Post, file: Express.Multer.File | undefined) => {
-  // todo generate image unique name
-  const imageName = file?.originalname + '_id';
+  let imageName = ''
+  if (file) {
+    const response = await uploadImage(file);
+    imageName = response?.secure_url!;
+  }
   // todo upload image to cloudinary
   await PostModel.create({ ...post, image: imageName, user: userId })
 }
