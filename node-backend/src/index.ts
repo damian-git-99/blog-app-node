ca07da34bb7e75fef7c6fa0551dce73b1302ca07
damian <damiangalvan66@gmail.com>
2023-03-28 11:06:19 -0600

    refactor(backend): move corsOptions to corsConfig file

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -7,16 +7,12 @@ import { connectDB } from './config/dbConfig';
 import { errorHandler } from './middlewares/errorHandler';
 import { userRouter } from './user/userRoutes';
 import { postRouter } from './posts/postRoutes';
+import { corsOptions } from './config/corsConfig';
 
 connectDB();
 const app = express();
-const port = 4000;
-const corsOptions = {
-  origin: 'http://127.0.0.1:5173',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-  credentials: true
-};
 
+const port = process.env.PORT || 4000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
