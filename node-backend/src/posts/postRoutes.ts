21f865bd2821fb79e27689b5e1749966aa809f64
damian <damiangalvan66@gmail.com>
2023-03-28 16:43:46 -0600

    refactor(backend): import all functions from postController using * and prefix function calls with postController.

--- node-backend/src/posts/postRoutes.ts
+++ node-backend/src/posts/postRoutes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import { requireAuth } from '../middlewares/requireAuth';
-import { createPost, getMyPosts, deletePostById, getPostById } from './postController';
+import * as postController from './postController';
 import { getRecentlyPublishedPosts } from './postController';
 import { checkToken } from '../middlewares/checkToken';
 const router = express.Router();
@@ -9,12 +9,12 @@ const upload = multer();
 
 router.post('/',
   [requireAuth, upload.single('file')],
-  createPost);
+  postController.createPost);
 
 router.get('/', getRecentlyPublishedPosts );
-router.get('/my-posts', requireAuth, getMyPosts );
-router.delete('/:id', requireAuth, deletePostById );
-router.get('/:id', checkToken,  getPostById );
+router.get('/my-posts', requireAuth, postController.getMyPosts );
+router.delete('/:id', requireAuth, postController.deletePostById );
+router.get('/:id', checkToken, postController. getPostById );
 
 export {
   router as postRouter
