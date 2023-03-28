b490a50420ed20253b000bbd02e3f5087010b435
damian <damiangalvan66@gmail.com>
2023-03-28 16:02:43 -0600

    refactor(backend): change constructor parameter name from message to postId and update message accordingly

--- node-backend/src/posts/errors/PostNotFound.ts
+++ node-backend/src/posts/errors/PostNotFound.ts
@@ -4,10 +4,10 @@ export class PostNotFound extends CustomError {
   statusCode: number;
   message: string;
 
-  constructor(message: string){
+  constructor(postId: string){
     super();
     this.statusCode = 404;
-    this.message = `Post Not found: ${message}`;
+    this.message = `Post Not found: ${postId}`;
   }
 
 }
