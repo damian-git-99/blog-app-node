c5571519b590001c8e41473a0d183e9e68fba34a
damian <damiangalvan66@gmail.com>
2023-03-31 11:02:36 -0600

    feat(backend): add support for localhost origin in corsOptions

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: 'http://127.0.0.1:5173',
+  origin: ['http://127.0.0.1:5173','http://localhost:5173'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
   credentials: true
 };
