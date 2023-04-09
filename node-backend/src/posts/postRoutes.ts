79ddd0e7ecd1963b780114736db6e9f703b475c2
damian <damiangalvan66@gmail.com>
2023-04-09 10:23:26 -0600

    refactor(backend): remove unused import of getRecentlyPublishedPosts from postController

--- node-backend/src/posts/postRoutes.ts
+++ node-backend/src/posts/postRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import multer from 'multer';
 import { requireAuth } from '../middlewares/requireAuth';
-import * as postController from './postController';
-import { getRecentlyPublishedPosts } from './postController';
 import { checkToken } from '../middlewares/checkToken';
+import * as postController from './postController';
+
 const router = express.Router();
 const upload = multer();
 
@@ -11,7 +11,7 @@ router.post('/',
   [requireAuth, upload.single('file')],
   postController.createPost);
 
-router.get('/', getRecentlyPublishedPosts );
+router.get('/', postController.getRecentlyPublishedPosts );
 router.get('/my-posts', requireAuth, postController.getMyPosts );
 router.delete('/:id', requireAuth, postController.deletePostById );
 router.get('/:id', checkToken, postController.getPostById );
