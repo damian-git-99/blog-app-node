947a8f5ad094dfcd53434b6ce8064f654e7f4f95
damian <damiangalvan66@gmail.com>
2023-04-14 17:59:26 -0600

    feat(backend): add validation to register and login routes using express-validator middleware

--- node-backend/src/auth/authRoutes.ts
+++ node-backend/src/auth/authRoutes.ts
@@ -1,9 +1,37 @@
 import express from 'express';
 import { login, logout, register } from './authController';
+import { validateFields } from '../middlewares/expressValidator';
+import { check } from 'express-validator';
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', 
+  [
+    check('email')
+      .notEmpty().withMessage('E-mail cannot be null')
+      .bail()
+      .isEmail().withMessage('E-mail is not valid'),
+    check('username')
+      .notEmpty().withMessage('Username cannot be null'),
+    check('password')
+      .notEmpty().withMessage('Password cannot be null'),
+    validateFields
+  ], 
+  register
+);
+
+router.post('/login',
+  [
+    check('email')
+      .notEmpty().withMessage('E-mail cannot be null')
+      .bail()
+      .isEmail().withMessage('E-mail is not valid'),
+    check('password')
+      .notEmpty().withMessage('Password cannot be null'),
+    validateFields
+  ], 
+  login
+);
+
 router.post('/logout', logout);
 
 export {
