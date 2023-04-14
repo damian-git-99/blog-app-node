bd99904c8291593baa3c9cd990143c057326605b
damian <damiangalvan66@gmail.com>
2023-04-14 10:39:24 -0600

    feat(frontend): add editProfile function to update user profile information

--- react-frontend/src/api/userApi.js
+++ react-frontend/src/api/userApi.js
@@ -18,3 +18,20 @@ export const userProfile = async () => {
     throw err
   }
 }
+
+export const editProfile = async (userId, user) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    withCredentials: true
+  }
+  try {
+    const { data } = await axios.put(`${URL}/profile/${userId}`, user, config)
+    return data
+  } catch (error) {
+    const message = error?.response?.data?.error || error.message
+    const err = new Error(message)
+    throw err
+  }
+}
