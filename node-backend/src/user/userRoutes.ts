c7ba21f15392d8deba7f3793c1ba44dc31157ac7
damian <damiangalvan66@gmail.com>
2023-04-14 10:38:58 -0600

    feat(backend): add editProfile function

--- node-backend/src/user/userRoutes.ts
+++ node-backend/src/user/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { userProfileController } from './userController';
+import { editProfile, userProfileController } from './userController';
 import { requireAuth } from '../middlewares/requireAuth';
 const router = express.Router();
 
-router.get('/profile', requireAuth ,userProfileController);
+router.get('/profile', requireAuth , userProfileController);
+router.put('/profile/:id', requireAuth , editProfile);
 
 export {
   router as userRouter
