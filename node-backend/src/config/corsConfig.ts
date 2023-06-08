754f84e4db6b8845ab52a25e3ce99126cf990bf8
damian <damiangalvan66@gmail.com>
2023-06-08 12:27:55 -0600

    refactor(corsConfig.ts): change cors origin to allow all domains

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -6,7 +6,7 @@ const cors_domain = process.env.cors_domain || ''
 console.log(cors_domain)
 
 export const corsOptions = {
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
+  origin: '*',
   credentials: false,
   methods: ['POST', 'PUT', 'GET', 'DELETE']
 }
