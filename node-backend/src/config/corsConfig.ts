d0c6db40cabadc1eef8261726fdeae198c4775da
damian <damiangalvan66@gmail.com>
2023-06-08 12:00:00 -0600

    chore(corsConfig.ts): remove console.log statement from corsConfig file

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -3,6 +3,8 @@ dotenv.config()
 
 const cors_domain = process.env.cors_domain || ''
 
+console.log(cors_domain)
+
 export const corsOptions = {
   origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
