356ec18e4d5189c8db1cd65da46056d7c2812007
damian <damiangalvan66@gmail.com>
2023-06-08 12:09:35 -0600

    feat(corsConfig.ts): add preflightContinue option to corsOptions

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -8,5 +8,6 @@ console.log(cors_domain)
 export const corsOptions = {
   origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-  credentials: true
+  credentials: true,
+  preflightContinue: true
 }
