f16c5e04408e18f981bf7e8015916ca5b1685123
damian <damiangalvan66@gmail.com>
2023-06-08 12:18:29 -0600

    feat(corsConfig.ts): add cors_domain environment variable to corsOptions origin array

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -6,7 +6,7 @@ const cors_domain = process.env.cors_domain || ''
 console.log(cors_domain)
 
 export const corsOptions = {
-  origin: '*',
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
   credentials: true,
   preflightContinue: true,
   methods: ['POST', 'PUT', 'GET', 'DELETE']
