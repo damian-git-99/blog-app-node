6a6cc47b55d9c31837bac8808dfc2d2d9797707e
damian <damiangalvan66@gmail.com>
2023-06-08 11:39:21 -0600

    feat(corsConfig.ts): add support for cors_domain environment variable in corsOptions origin array

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -1,5 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const cors_domain = process.env.cors_domain || ''
+
 export const corsOptions = {
-  origin: ['http://127.0.0.1:5173','http://localhost:5173'],
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
   credentials: true
-};
+}
