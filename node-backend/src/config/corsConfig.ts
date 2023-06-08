bc55bf68e06c958cf8fa16ea5fe8079d3a25bf0f
damian <damiangalvan66@gmail.com>
2023-06-08 11:48:42 -0600

    fix(corsConfig.ts): set credentials option to false to prevent sending cookies and authentication headers to other domains

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -6,5 +6,5 @@ const cors_domain = process.env.cors_domain || ''
 export const corsOptions = {
   origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-  credentials: true
+  credentials: false
 }
