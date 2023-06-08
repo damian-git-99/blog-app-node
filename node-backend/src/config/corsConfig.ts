48d7c15510bcf63d1ee89d8d4ab5efa8181e93ab
damian <damiangalvan66@gmail.com>
2023-06-08 11:47:45 -0600

    fix(corsConfig.ts): use nullish coalescing operator to set default value for cors_domain variable

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const cors_domain = process.env.cors_domain || ''
+const cors_domain = process.env.cors_domain ?? ''
 
 export const corsOptions = {
   origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
