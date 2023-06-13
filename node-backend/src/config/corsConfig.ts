244b38284b8482cc93e756099b31bba13321df07
damian <damiangalvan66@gmail.com>
2023-06-13 12:47:09 -0600

    feat(corsConfig.ts): add default value for cors_domain if not provided in environment variables. Update corsOptions to allow all HTTP methods.

--- node-backend/src/config/corsConfig.ts
+++ node-backend/src/config/corsConfig.ts
@@ -1,10 +1,10 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const cors_domain = process.env.cors_domain ?? ''
+const cors_domain = process.env.cors_domain || 'http://localhost:5173'
 
 export const corsOptions = {
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173', cors_domain],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-  credentials: true
+  origin: cors_domain,
+  credentials: true,
+  methods: ['POST', 'PUT', 'GET', 'DELETE']
 }
