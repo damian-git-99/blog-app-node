42d3340183efa256af5fb3d9b2f85a9821d87c1e
damian <damiangalvan66@gmail.com>
2023-06-19 09:54:30 -0600

    feat(userController.ts, userRoutes.ts): add endpoints for adding and deleting favorite posts refactor(userController.ts, userRoutes.ts): apply code formatting and remove unnecessary semicolons

--- node-backend/src/user/userController.ts
+++ node-backend/src/user/userController.ts
@@ -1,24 +1,40 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 import * as userService from './userService'
 
 //@route Get users/profile
 export const userProfileController = async (req: Request, res: Response) => {
-  const id = req.currentUser?.id!;
-  const user = await userService.userProfile(id);
-  res.status(200)
-    .json({
-      email: user.email,
-      username: user.username,
-      id: user.id
-    })
+  const id = req.currentUser?.id!
+  const user = await userService.userProfile(id)
+  res.status(200).json({
+    email: user.email,
+    username: user.username,
+    id: user.id
+  })
 }
 
 //@route PUT users/profile/:id
 export const editProfile = async (req: Request, res: Response) => {
-  const { username, email, password } = req.body;
-  const currentUser = req.currentUser!;
-  const userId = req.params.id;
-  await userService.editProfile(currentUser, userId, { username, email, password });
-  res.status(200)
-    .json('ok')
+  const { username, email, password } = req.body
+  const currentUser = req.currentUser!
+  const userId = req.params.id
+  await userService.editProfile(currentUser, userId, {
+    username,
+    email,
+    password
+  })
+  res.status(200).json('ok')
+}
+
+//@route PUT users/add-favorite-post/:postId
+export const addFavoritePost = async (req: Request, res: Response) => {
+  const userId = req.currentUser!.id
+  await userService.addFavoritePost(userId, req.params.postId)
+  res.status(200).json('ok')
+}
+
+//@route PUT users/delete-favorite-post/:postId
+export const deleteFavoritePost = async (req: Request, res: Response) => {
+  const userId = req.currentUser!.id
+  await userService.deleteFavoritePost(userId, req.params.postId)
+  res.status(200).json('ok')
 }
--- node-backend/src/user/userRoutes.ts
+++ node-backend/src/user/userRoutes.ts
@@ -1,22 +1,27 @@
-import express from 'express';
-import { editProfile, userProfileController } from './userController';
-import { requireAuth } from '../middlewares/requireAuth';
-import { validateFields } from '../middlewares/expressValidator';
-import { body } from 'express-validator';
-const router = express.Router();
+import express from 'express'
+import {
+  editProfile,
+  userProfileController,
+  addFavoritePost,
+  deleteFavoritePost
+} from './userController'
+import { requireAuth } from '../middlewares/requireAuth'
+import { validateFields } from '../middlewares/expressValidator'
+import { body } from 'express-validator'
+const router = express.Router()
 
-router.get('/profile', requireAuth , userProfileController);
-router.put('/profile/:id', 
+router.get('/profile', requireAuth, userProfileController)
+router.put(
+  '/profile/:id',
   [
-    body('email')
-      .optional()
-      .isEmail()
-      .withMessage('E-mail is not valid'),
+    body('email').optional().isEmail().withMessage('E-mail is not valid'),
     validateFields
-  ], 
-  requireAuth , editProfile
-);
+  ],
+  requireAuth,
+  editProfile
+)
 
-export {
-  router as userRouter
-}
+router.post('/add-favorite-post/:postId', requireAuth, addFavoritePost)
+router.delete('/delete-favorite-post/:postId', requireAuth, deleteFavoritePost)
+
+export { router as userRouter }
