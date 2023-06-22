7655fbcb32454428f5eab8fbab499a863095597c
damian <damiangalvan66@gmail.com>
2023-06-22 12:03:12 -0600

    style(PostEntry.jsx): remove unnecessary newline character

--- react-frontend/src/components/PostEntry.jsx
+++ react-frontend/src/components/PostEntry.jsx
@@ -9,6 +9,7 @@ export const PostEntry = ({ post }) => {
   const image = post.image
     ? post.image
     : 'https://images.unsplash.com/photo-1628155930542-3c7a64e2c833?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+
   const handleClick = (e) => {
     e.stopPropagation()
     navigate('/post/' + post._id)
