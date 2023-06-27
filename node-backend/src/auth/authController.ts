38392e35db5e9b09516f52bbe57dd757b0f373a1
damian <damiangalvan66@gmail.com>
2023-06-27 09:44:26 -0600

    feat(authController.ts, main.hbs, reset-password-success.hbs, reset-password.hbs): add password reset functionality and views
    Add password reset functionality to the authController.ts file, which includes a resetPasswordCheck function and a resetPassword function. The resetPasswordCheck function checks if the token is valid, and the resetPassword function resets the user's password.
    
    Add three new views to the views folder: reset-password.hbs, reset-password-success.hbs, and main.hbs. The reset-password.hbs view displays a form for the user to enter their new password. The reset-password-success.hbs view displays a success message and a link to return to the application. The main.hbs file is a layout file that includes a Bootstrap stylesheet.
    
    The resetPassword function also includes a frontend_url variable that is set to the frontend domain or a default localhost URL. This variable is used in the reset-password-success.hbs view to display a link to return to the

--- node-backend/src/auth/authController.ts
+++ node-backend/src/auth/authController.ts
@@ -69,7 +69,7 @@ export const recoverPassword = async (req: Request, res: Response) => {
 export const resetPasswordCheck = async (req: Request, res: Response) => {
   const { token } = req.params
   await authService.resetPasswordCheck(token)
-  res.status(200).json('ok')
+  res.render('reset-password', { token })
 }
 
 //@route POST /reset-password/:token
@@ -77,5 +77,8 @@ export const resetPassword = async (req: Request, res: Response) => {
   const { password } = req.body
   const { token } = req.params
   await authService.resetPassword(token, password)
-  res.status(200).json('ok')
+  const frontend_url = `${
+    process.env.frontend_domain || 'http://localhost:5173'
+  }/login`
+  res.render('reset-password-success', { frontend_url })
 }
