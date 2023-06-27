4d65c1c7de4f2a53b1c762746c76bb750494397f
damian <damiangalvan66@gmail.com>
2023-06-27 09:43:47 -0600

    style(index.ts): fix indentation and add comments to the code feat(index.ts): add view engine configuration to the app

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,25 +1,39 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import { engine } from 'express-handlebars'
+import path from 'path'
+import cors from 'cors'
 import 'express-async-errors'
-import cookieParser from 'cookie-parser';
-import { authRouter } from './auth/authRoutes';
-import { connectDB } from './config/dbConfig';
-import { errorHandler } from './middlewares/errorHandler';
-import { userRouter } from './user/userRoutes';
-import { postRouter } from './posts/postRoutes';
-import { corsOptions } from './config/corsConfig';
+import cookieParser from 'cookie-parser'
+import { authRouter } from './auth/authRoutes'
+import { connectDB } from './config/dbConfig'
+import { errorHandler } from './middlewares/errorHandler'
+import { userRouter } from './user/userRoutes'
+import { postRouter } from './posts/postRoutes'
+import { corsOptions } from './config/corsConfig'
 
-connectDB();
-const app = express();
+connectDB()
+const app = express()
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-app.use(authRouter);
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
-app.use(errorHandler);
+// view config
+app.set('views', path.join(__dirname, 'views'))
+app.engine(
+  'hbs',
+  engine({
+    extname: 'hbs'
+  })
+)
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: false }))
+// view config
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use(authRouter)
+app.use('/users', userRouter)
+app.use('/posts', postRouter)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
