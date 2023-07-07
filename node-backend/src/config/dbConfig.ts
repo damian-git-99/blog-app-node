ef44cf2c7b6fc03e7f73da017bc7930cc95b96f6
damian <damiangalvan66@gmail.com>
2023-07-07 09:29:38 -0600

    fix(dbConfig.ts): fix import statement for logger fix(dbConfig.ts): remove unnecessary console.log statement feat(dbConfig.ts): add logging for successful and unsuccessful database connection

--- node-backend/src/config/dbConfig.ts
+++ node-backend/src/config/dbConfig.ts
@@ -1,15 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
+import { logger } from './logger'
 dotenv.config()
 
-const URI = process.env.MONGO_URI;
+const URI = process.env.MONGO_URI
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(URI!);
-    console.log('The connection with the database was successful');
+    await mongoose.connect(URI!)
+    logger.info('The connection with the database was successful')
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    logger.error('The connection with the database was unsuccessful: ' + error)
+    process.exit(1)
   }
-};
+}
