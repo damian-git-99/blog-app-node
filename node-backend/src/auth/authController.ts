00d25214b4be9b2d342b3164fb81b49493793e8d
damian <damiangalvan66@gmail.com>
2023-07-07 10:50:05 -0600

    fix(authController.ts): remove unnecessary logging statements feat(authController.ts): add logging statements for user registration, login, logout, token verification, password recovery, and password reset

--- node-backend/src/auth/authController.ts
+++ node-backend/src/auth/authController.ts
@@ -8,9 +8,9 @@ import { logger } from '../config/logger'
 //@route Post /register
 export const register = async (req: Request, res: Response) => {
   const { email, password, username } = req.body
+  logger.info(`User registered request`)
   const user = await authService.registerUser({ email, password, username })
   const token = generateToken({ id: user.id, email: user.email })
-  logger.info(`User registered: ${user.email}`)
   res
     .status(201)
     .cookie('token', token, { sameSite: 'none', secure: true })
@@ -25,9 +25,9 @@ export const register = async (req: Request, res: Response) => {
 //@route Post /login
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body
+  logger.info(`User logged in request: ${email}`)
   const user = await authService.login({ email, password })
   const token = generateToken({ id: user.id, email: user.email })
-  logger.info(`User logged in: ${user.email}`)
   res
     .status(200)
     .cookie('token', token, { sameSite: 'none', secure: true })
@@ -41,7 +41,7 @@ export const login = async (req: Request, res: Response) => {
 //@route Post /logout
 export const logout = async (req: Request, res: Response) => {
   const token = req.cookies['token']
-  logger.info(`User logged out: ${token}`)
+  logger.info(`User logged out request: ${token}`)
   res
     .cookie('token', null, { sameSite: 'none', secure: true, signed: false })
     .json('ok')
@@ -52,8 +52,8 @@ export const verifyToken = async (req: Request, res: Response) => {
   const userId = req.currentUser?.id!
   const user = await getUserById(userId)
   if (!user) throw new UserNotFound()
+  logger.info(`User verified request: ${user.email}`)
   const token = generateToken({ id: user.id, email: user.email })
-  logger.info(`User verified: ${user.email}`)
   res
     .status(200)
     .cookie('token', token, { sameSite: 'none', secure: true, signed: false })
@@ -67,16 +67,16 @@ export const verifyToken = async (req: Request, res: Response) => {
 //@route POST /recover-password
 export const recoverPassword = async (req: Request, res: Response) => {
   const { email } = req.body
-  await authService.recoverPassword(email)
   logger.info(`Password recovery requested for email: ${email}`)
+  await authService.recoverPassword(email)
   res.status(200).json('ok')
 }
 
 //@route GET /reset-password/:token
 export const resetPasswordCheck = async (req: Request, res: Response) => {
   const { token } = req.params
+  logger.info(`Password reset request: ${token}`)
   await authService.resetPasswordCheck(token)
-  logger.info(`Password reset token verified for token: ${token}`)
   res.status(200).json('ok')
 }
 
@@ -84,7 +84,7 @@ export const resetPasswordCheck = async (req: Request, res: Response) => {
 export const resetPassword = async (req: Request, res: Response) => {
   const { password } = req.body
   const { token } = req.params
+  logger.info(`Password reset request for token: ${token}`)
   await authService.resetPassword(token, password)
-  logger.info(`Password reset for token: ${token}`)
   res.status(200).json('ok')
 }
