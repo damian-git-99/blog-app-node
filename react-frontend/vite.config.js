ff11b3de6947064cd25820a42cae2a1398eb9bea
damian <damiangalvan66@gmail.com>
2023-10-02 17:00:51 -0600

    chore(package.json): add vite-plugin-purgecss as a dependency chore(vite.config.js): add htmlPurge plugin to the list of plugins

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import htmlPurge from 'vite-plugin-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPurge([htmlPurge()])],
   server: {
     watch: {
       usePolling: true
