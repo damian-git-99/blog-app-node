75cef107c5a75e04614a4448681d87b8f2737de2
damian <damiangalvan66@gmail.com>
2023-10-23 10:04:03 -0600

    test(shared.ts): add mock for ImageService and its methods

--- node-backend/__tests__/unit/shared.ts
+++ node-backend/__tests__/unit/shared.ts
@@ -1,6 +1,7 @@
 import { JWTService } from '../../src/auth/jwt/JWTService'
 import { PasswordEncoder } from '../../src/auth/passwordEncoder/PasswordEncoder'
 import { EmailService } from '../../src/shared/email/EmailService'
+import { ImageService } from '../../src/shared/image/ImageService'
 
 export let jwtServiceMockShared: JWTService = {
   generateToken: jest.fn((payload, expiresIn) => {
@@ -25,4 +26,17 @@ export let passwordEncoderMockShared: PasswordEncoder = {
 
 export let emailServiceMockShared: EmailService = {
   sendEmail: jest.fn(async (email, subject, message) => {})
+}
+
+export let imageServiceMockShared: ImageService = {
+  uploadImage: jest.fn(async (image) => {
+    return {
+      imageId: '1',
+      url: 'imageURL'
+    }
+  }),
+  deleteImage: jest.fn(async (image) => {}),
+  getImageURL: jest.fn((image) => {
+    return 'imageURL'
+  })
 }
